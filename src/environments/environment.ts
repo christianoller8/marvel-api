dc3caa5b34969fc6a34a6586762b4a8b3c4af729
Christian <christianoller8@gmail.com>
2024-05-20 23:49:20 +0200

    https fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   name: "",
   code: 0,
-  url: "http://gateway.marvel.com/v1/public/series",
+  url: "https://gateway.marvel.com/v1/public/series",
   publicKey: "ebe435710d107a3a1fc3115018320a9c",
   privateKey: "731731ac3c6604612f67b344c7ecdff50e66b8d7",
 };
